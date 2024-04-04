aa1e3a08bd0fbd1e2046e6c01e6dba8a443e5608
Diana-Paola03 <dianap.avendanod@ecci.edu.co>
2024-04-04 16:08:12 -0500

    hola

--- Taller_2/Ejercicio_B3_ui.py
+++ Taller_2/Ejercicio_B3_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Ejercicio_3A_ui.ui'
+# Form implementation generated from reading ui file 'Ejercicio_3B_ui.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
